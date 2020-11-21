424c978f8944ebe8adb6daf760d08423f1325da7
moises <moisesalex132@gmail.com>
2020-11-21 17:21:42 -0600

    Configuraciones para subida de imagenes y validacion

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const express = require('express');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
 
-const dotenv = require('dotenv');
+const multer = require('multer');
 
-const sequelize = require('./database');
+//unique id generator
+const { v4: uuidv4 } = require('uuid');
+
+//initialization
 const app = express();
 
+//Database
+const sequelize = require('./database');
+
+//env settings
 require('./config/index.config');
 
 //settings 
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/images'),
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + path.extname(file.originalname).toLowerCase());
+    },
+})
 app.engine('.hbs', handlebars({
     extname: '.hbs',
     defaultLayout: 'main',
@@ -25,20 +39,38 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(multer({
+    storage: storage,
+    dest: path.join(__dirname, 'public/images'),
+    limits: { fieldSize: 1000000 },
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif|svg/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+
+        cb("error: Archivo debe ser una imagen valida");
+    }
+}).single('image'))
+
 dotenv.config();
+
 
 //Routes
 const usuario = require('./models/user.model');
 
 app.get('/', (req, res) => {
-    // res.render('index.hbs');
-
-     usuario.create({
-         name:'ELIEL',
-         email:"ELIEL21@gmail.com",
-         password:'12314'
-     }).then(user => res.json(user));
+    res.render('index.hbs')
 })
+
+app.post('/upload', (req, res) => {
+    console.log(req.file);
+    res.send('Upload')
+})
+
 
 app.use(require('./routes/api.routes'));
 
@@ -50,9 +82,9 @@ app.listen(process.env.PORT || 4000, () => {
     console.log('Server on PORT 4000');
 
     // connection to the database
-    sequelize.sync({force:false}).then(()=>{
+    sequelize.sync({ force: false }).then(() => {
         console.log('connected database');
-    }).catch((err)=>{
-        console.log('An error has occurred',err);
+    }).catch((err) => {
+        console.log('An error has occurred', err);
     })
 })
